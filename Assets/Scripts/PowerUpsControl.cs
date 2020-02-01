bc6c3a17b7fc176825ad6dae7151549b997588ab
Raoni <raonicv17@outlook.com>
2020-02-01 15:27:51 -0300

    PowerUps Start

---
+++ Assets/Scripts/PowerUpsControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpsControl : MonoBehaviour
+{
+    public bool _canClicker = false;
+    public bool _canShield = false;
+    public bool _canHelpOfStars = false;
+    public bool _canFreeze = false;
+    public bool _canDontGiveUp = false;
+    public bool _canChristimas = false;
+    public bool _canZordTime = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickerPowerupOn() {
+        
+
+        if (_canClicker == true)
+        {
+
+        }
+
+    }
+
+    public void ShieldPowerupOn() { }
+
+    public void HelpOfStarsPowerupOn() { }
+
+    public void FreezePowerupOn() { }
+
+    public void DontGiveUpPowerupOn() { }
+
+    public void ChristimasPowerupOn() { }
+
+    public void ZordTimePowerupOn() { }
+}
